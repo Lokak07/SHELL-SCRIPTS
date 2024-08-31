8a14be5e3a5a301318fb0938dba1c066ac3ebd10
Lokak07 <lokashakthi19.ls@gmail.com>
2024-08-31 12:01:01 +0530

    adding code for DAY3

---
+++ DAY-3/logfile.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Set the directory where logs will be stored
+LOG_DIRECTORY="/home/loka/DEVOPS-PROJECTS/SHELL_SCRIPTING/SHELL-SCRIPTS/DAY-3/log"
+
+
+#date in current format
+current_date=$(date +"%d-%m-%Y")
+
+
+#saves the log file in this format
+LOG_FILE="$LOG_DIRECTORY/log_$current_date.txt"
+
+
+#write a message to the log file
+echo "log entry for $current_date" > "$LOG_FILE"
+echo ">>>>>>>>>>>>>>>>>>>>>>>>>>>" >> "$LOG_FILE"
+echo "this is a sample log entry" >> "$LOG_FILE"
+echo "logged at $(date +"%H-%M-%S")" >> $LOG_FILE
+echo "cheen tapaku dum dum" >> $LOG_FILE
+
+#log file saved
+
+echo "log file saved at $LOG_FILE" 
